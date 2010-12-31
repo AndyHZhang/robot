c385899307d7fb915191d3df5695540d18496577
Andy.H.Zhang <Andy.H.Zhang@qisda.com>
2010-12-31 10:53:29 +0800

    init Evaluation

---
+++ Evaluation/src/com/robot/evaluation/Action.java
@@ -0,0 +1,13 @@
+package com.robot.evaluation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Action extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.action);
+	}
+}
